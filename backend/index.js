8ce63f846f28c99339d17f22fd702bcc5b648986
melitacasola <melissacasola@gmail.com>
2024-02-21 20:13:53 +0100

    chore: update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,6 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hola desde el servidor!" });
-
-});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
