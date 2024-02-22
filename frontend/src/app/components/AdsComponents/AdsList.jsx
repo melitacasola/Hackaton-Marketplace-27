5bc303ec5357bfff6720dc37daa61a02a283d456
melitacasola <melissacasola@gmail.com>
2024-02-22 20:38:13 +0100

    feat: styles home page fixed

--- frontend/src/app/components/AdsComponents/AdsList.jsx
+++ frontend/src/app/components/AdsComponents/AdsList.jsx
@@ -1,58 +1,21 @@
 "use client"
-
-import { useEffect, useState } from 'react';
 import AdsComponent from "./AdsComponent"
+import { useFetchApi } from '@/app/utils/useFetchApi';
 
 
 
 const AdsList = () => {
+    const urlApi = 'http://localhost:3200/api/v1/coders-stack'
 
-   /* let ads_client = [
-        {
-        "id": "01",
-        "name": "JUAN", 
-        "age" : 25,  
-        "profession":"Developer",
-        "advertisement":"miprofiolllesta en linkedin pasate a verlos",
-    },
-    {
-        
-        "id": "04",
-        "name": "TANIA",
-        "age" :30,
-        "profession":"Designer",
-        "advertisement":"Busco un trabajo en una empresa que me permita crecer como diseñadora."
-    
-    },
-    {
-        "id": "05",
-        "name": "PEDRO",
-        "age" :45,
-        "profession":"Manager", 
-        "advertisement":"Estoy buscando un puesto para mi nuevo equipo. Me interesa trabajar con gente joven y con experiencia."
+    const { ads, loading, error } = useFetchApi(urlApi);
+
+    if (loading) {
+        return <div>Cargando...</div>;
     }
-    ]*/
 
-    const [ads, setAds] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    const fetchAds = async () => {
-        try {
-            const response = await fetch('http://localhost:3200/api/v1/coders-stack')
-            const data = await response.json()
-            console.log(data);
-            setAds(data.data)
-            setLoading(false)
-        } catch (error) {
-            setError(error)
-            setLoading(false)
-        }
+    if (error) {
+        return <div>Error: {error.message}</div>;
     }
-    useEffect(() => {
-       
-        fetchAds()
-    }, [])
-
 
 
     return (
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import AdsList from "./components/AdsComponents/AdsList";
+import Search from "./components/Search/Search";
 
-export default function Home() {
+export default function Home({searchParams}) {
+  const query = searchParams?.query
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <AdsList />
+    <main className="flex min-h-screen flex-col items-center p-6">
+      <Search />
+      <AdsList  query={query} />
     </main>
   );
 }
